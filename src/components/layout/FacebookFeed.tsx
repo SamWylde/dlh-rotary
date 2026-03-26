ee542aa2bea554919395a235f5bb82101d4dd944
SamWylde <tdarbylhu@gmail.com>
2026-03-26 16:55:05 -0400

    Redesign Facebook feed as compact side-by-side layout
    Shrink iframe (340x380, small header, no cover/facepile),
    two-column layout with text+buttons left and feed right,
    stacks on mobile.

--- src/components/layout/FacebookFeed.tsx
+++ src/components/layout/FacebookFeed.tsx
@@ -3,9 +3,9 @@
 export const FacebookFeed = () => (
   <div className="flex justify-center">
     <iframe
-      src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fprofile.php%3Fid%3D100064347773545&tabs=timeline&width=500&height=600&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true"
-      width="500"
-      height="600"
+      src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fprofile.php%3Fid%3D100064347773545&tabs=timeline&width=340&height=380&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=false"
+      width="340"
+      height="380"
       style={{ border: 'none', overflow: 'hidden', maxWidth: '100%' }}
       scrolling="no"
       frameBorder="0"
